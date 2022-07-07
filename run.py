876d23909603010d30cdad5ff513a44c34321da2
teresabowe <teresa.bowe@gmail.com>
2022-07-07 09:56:31 +0000

    Create class to represent analytics data. Create functions to input data

--- run.py
+++ run.py
@@ -12,9 +12,37 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('website_data')
 
-dataset = SHEET.worksheet('dataset')
+class TimePeriod:
+    """
+    Represent website analytics data for a time period.
+    """
+    def __init__(self, visits, pageviews, orders, revenue):
+        self.visits = visits
+        self.orders = orders
+        self.pageviews = pageviews
+        self.revenue = revenue
 
-data = dataset.get_all_values()
+    def get_entered_as_list(self):
+        return[self.visits, self.pageviews, self.orders, self.revenue]
 
-print(data)
+def get_data_item(type):
+    """
+    Get input from user for data.
+    """
+    input_data = input(f"Enter your {type} data here:\n")
+    return(int(input_data))
 
+def gather_data():
+    """
+    Assign variable to data collection function.
+    """       
+    visits_data = get_data_item("visits")
+    pageviews_data = get_data_item("pageviews")
+    orders_data = get_data_item("orders")
+    revenue_data = get_data_item("revenue")
+
+    return TimePeriod(visits_data, pageviews_data, orders_data, revenue_data)
+
+day_of_data = gather_data()
+list_entered = day_of_data.get_entered_as_list()
+print(list_entered)

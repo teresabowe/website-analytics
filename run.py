5ba681b8e7b3b3dc0eb3433fb587fb8006c0af4b
teresabowe <teresa.bowe@gmail.com>
2022-07-08 10:13:41 +0000

    Add new function with parameters to call historical data retrieval function

--- run.py
+++ run.py
@@ -97,7 +97,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
 
-def get_last_7_entries_dataset():
+def get_historical_entries_dataset(param1, param2):
     """
     Collects columns of data from the dataset worksheet, collecting
     the last 7 entries and returns the data as a list of lists.
@@ -107,10 +107,19 @@ def get_last_7_entries_dataset():
     columns = []
     for ind in range(1, 5):
         column = seven_days.col_values(ind)
-        columns.append(column[-14:-7])
+        columns.append(column[param1:param2])
 
-    return columns
-    
+    return columns 
+
+def gather_historical_data():
+    """
+    Assign variable to historical data collection function.
+    """
+    historical_data_14_days = get_historical_entries_dataset(-14, -7)
+    historical_data_7_days = get_historical_entries_dataset(-7, None)
+
+    return historical_data_14_days, historical_data_7_days
+
 def main():
     """
     Run program functions
@@ -122,5 +131,7 @@ def main():
     print(list_for_sheet)
     update_worksheet(list_for_sheet, "dataset")
     
-dataset_columns = get_last_7_entries_dataset()
-print(dataset_columns)
+historical_data_14_days = get_historical_entries_dataset(-14, -7)
+historical_data_7_days = get_historical_entries_dataset(-7, None)
+print(historical_data_14_days)
+print(historical_data_7_days)
